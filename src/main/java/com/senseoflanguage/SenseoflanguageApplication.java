af70e74831cc9f59169003e9e23cf5e4b2c280c1
Ivan Polahniuk <ivan_polahniuk@epam.com>
2020-04-15 01:18:00 +0300

    initial commit

---
+++ src/main/java/com/senseoflanguage/SenseoflanguageApplication.java
@@ -0,0 +1,13 @@
+package com.senseoflanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseoflanguageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenseoflanguageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senseoflanguage/SenseoflanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senseoflanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenseoflanguageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
